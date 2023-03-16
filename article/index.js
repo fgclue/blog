069c1ca029f76ba517822e2232bd4360df874ded
fgclue <schefferdaudtfelipe@gmail.com>
2023-03-16 18:00:38 -0300

    +New Post

--- article/index.js
+++ article/index.js
@@ -49,6 +49,24 @@ if (id == 1) {
     });
 }
 
+
+if (id == 2) {
+  fetch('/article/article.json')
+    .then(response => response.json())
+    .then(data => {
+      const article = data.articles['2'];
+      const title = article.title;
+      const location = article.location;
+      fetch(location)
+        .then(response => response.text())
+        .then(markdown => {
+            const titleHtml = `<h1 articlepreview>${title}</h1>`;
+            articleTitle.innerHTML = titleHtml;
+            articleContent.innerHTML = parseMarkdown(markdown);
+        });
+    });
+}
+
 /*
 I have a JSON file called article.json with a table called articles. Inside that table there is a table called 1 and 2. Inside these tables there is a title and location tag. Title is the title to the blog post and location is pointed to "/articles/1.md" and "/articles/2.md". I want to fetch the id 1 and set the articleTitle to the title and articleContent to the content of the Markdown file. My JS code is `var articleTitle = document.getElementById("title");
 var articleContent = document.getElementById("content");
